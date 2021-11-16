121bd24ad8fe52b6f497f82b548060547e39f0f7
Matt Blair <mblair@rivian.com>
2021-11-15 20:37:35 -0800

    cute little style update

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
       </div>
       <nav className="flex items-center justify-between flex-wrap bg-opacity-100 p-4">
         <div className="flex items-center flex-shrink-0 text-xl text-grey-900 mr-6">
-          <a href="https://www.solcapture.com" className="lg:inline-block">
+          <a href="/" className="lg:inline-block">
             <img alt="SolCapture" className="h-14 mr-3 lg:inline-block" src="/img/logo/0x0.png" />
             <div className="lg:inline-block">SolCapture</div>
           </a>
