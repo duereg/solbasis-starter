e03636fae9265ccfcb6fffc3146ddc315c46d358
Matt Blair <mblair@rivian.com>
2021-11-15 20:45:20 -0800

    small language cleanup

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,6 @@ import Footer from '@components/Footer'
 
 export default function About() {
   return (
-
     <div className="flex flex-col min-h-screen">
     <main className="flex-grow">
       <Head>
--- pages/why-us.js
+++ pages/why-us.js
@@ -22,9 +22,9 @@ export default function About() {
         <div className="mx-3 col-span-3 lg:col-span-2 px-2">
           <h1 className="title text-5xl mb-4">Why do you need our service?</h1>
           <div className="content py-1">
-            <p>Staking rewards can be earned every 2-4 days on the Solana blockchain. Each reward is currently viewed as a taxable event and treated as ordinary income.
-              When the reward is earned, we record the cost basis of the reward which can be used at a later date should a user decide to sell their token.
-              Our reporting process allows individuals to plan their tax year whether that means projecting quarterly estimated tax payments or recording all taxable transactions at the end of the year. </p>
+            <p>You can earn staking rewards every 2-4 days on the Solana blockchain. Each reward is currently viewed as a taxable event and treated as ordinary income.
+              When the reward is earned, our service records the cost basis of the reward which can be used at a later date should a user decide to sell their token.
+              Our reporting process allows individuals to plan their tax year whether that means projecting quarterly estimated tax payments or recording all taxable transactions at the end of the year.</p>
             <p>The sale of a token is currently considered a taxable event for U.S. citizens and a taxpayer needs to have the cost basis to determine if a realized gain or loss was incurred.
               The cost basis is the original value or purchase price and date of an asset, in this case Sol.
               With the reports we provide, users aren’t left guessing when the reward was earned or the value of the reward at the time it was earned.
