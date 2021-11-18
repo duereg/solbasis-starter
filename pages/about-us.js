476f0216df90774eede30c9f2dd09ff4098ff8d4
Matt Blair <mblair@rivian.com>
2021-11-18 08:54:53 -0800

    more cute bio rewrites

--- pages/about-us.js
+++ pages/about-us.js
@@ -24,7 +24,7 @@ export default function About() {
           <div className="mx-3 col-span-3 lg:col-span-2 px-2">
             <h1 className="title text-5xl mb-4">About</h1>
             <div className="content py-1">
-              <p>SolCapture is founded by Accounting and Tech Professionals.We offer specialized accounting analytic services within the Solana Blockchain.We currently offer a staking reward monitoring service which tracks taxable events related to staking rewards.</p>
+              <p>SolCapture is founded by Accounting and Tech Professionals. We offer specialized accounting analytic services within the Solana Blockchain. We currently offer a staking reward monitoring service which tracks taxable events related to staking rewards.</p>
             </div>
             <h1 className="title text-4xl mb-4">Founding Team</h1>
             <div className="content py-1">
@@ -36,8 +36,8 @@ export default function About() {
                 </div>
                 <div className="bio-wrapper">
                   <p><a href="https://www.linkedin.com/in/andrew-creedon-cpa-cfe-0741b123/">Andrew Creedon</a> is an early Solana Validator, Certified Public Accountant, and Certified Fraud Examiner. </p>
-                  <p>Mr. Creedon has developed complex financial models to accurately identify various assets and apply different accounting protocols to properly record and recognize each asset.</p>
-                  <p>Mr. Creedon specializes in financial analysis and accounting services across a variety of industries. </p>
+                  <p>Developer of complex financial models to accurately identify various assets and apply different accounting protocols to properly record and recognize each asset.</p>
+                  <p>Specialist in financial analysis and accounting services across a variety of industries. </p>
                 </div>
               </div>
               <div className="row-wrapper">
@@ -47,11 +47,9 @@ export default function About() {
                   </div>
                 </div>
                 <div className="bio-wrapper">
-                  <p><a href="https://www.linkedin.com/in/mattblair/">Matt Blair</a> is a tech professional and Open Source software enthusiast in the Bay Area,
-                    author of libraries such as <a href="https://github.com/duereg/js-algorithms">js-algorithms</a>, <a href="https://github.com/duereg/songbird">songbird</a>,
-                    contributor to projects such as <a href="https://github.com/emberjs/ember.js">ember</a> and <a href="https://github.com/emberjs/data">ember-data</a>,
-                    as well as a maintainer of <a href="https://github.com/btford/write-good">write-good</a>.</p>
-                  <p>You can read some of his <a href="https://blog.mattblair.co/">industry musings here</a>, or see him present at technical meetups and <a href="https://www.youtube.com/watch?v=WiANbp0alPs">conferences</a>.</p>
+                  <p><a href="https://www.linkedin.com/in/mattblair/">Matt Blair</a> is a tech professional and Open Source software enthusiast in the Bay Area.
+                    Industry experience building teams and software systems around legal compliance, monetization, identity & security. </p>
+                  <p>You can read some of his <a href="https://blog.mattblair.co/">industry musings here</a>, see his <a href="https://github.com/duereg">open source contributions</a>, or see him present at technical meetups and <a href="https://www.youtube.com/watch?v=WiANbp0alPs">conferences</a>.</p>
                   <p>Alumni of such companies as Stripe, Slack, and Rivian</p>
                 </div>
               </div>
@@ -62,7 +60,9 @@ export default function About() {
                   </div>
                 </div>
                 <div className="bio-wrapper">
-                  <p><a href="https://www.linkedin.com/in/dagren/">Daniel Agren</a> is a tech professional specializing in Virtualization, Hot/Cold Storage, and the Solana ecosystem including validators. He works in computer hardware fundamentals related to virtualized environments such as OpenStack and VMware. His specialities relate to information defense technologies and information security.</p>
+                  <p><a href="https://www.linkedin.com/in/dagren/">Daniel Agren</a> is a tech professional experienced in Virtualization, Hot/Cold Storage, and the Solana ecosystem including validators.</p>
+                  <p> Industry experience in computer hardware fundamentals related to virtualized environments such as OpenStack and VMware. </p>
+                  <p> Specialities relate to information defense technologies and information security.</p>
                 </div>
               </div>
             </div>
