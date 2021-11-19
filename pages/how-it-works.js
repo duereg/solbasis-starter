e6d210b8edcdac9986b65f46ab22ec797c6fb1a0
Matt Blair <mblair@rivian.com>
2021-11-18 16:26:58 -0800

    cleanup of how it works

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -21,13 +21,18 @@ export default function About() {
         <div className="mx-3 col-span-3 lg:col-span-2 px-2">
           <h1 className="title text-5xl">How it works</h1>
           <div className="content py-1">
+            <h2>What you will do:</h2>
             <ol>
               <li>Sign up with SolCapture and we'll analyze the Solana blockchain on your behalf.</li>
+            </ol>
+            <h2>What we will do:</h2>
+            <ol>
               <li>We apply accounting protocols to the Solana blockchain.</li>
               <li>We compute results applying different cost basis methodologies.</li>
               <li>We provide cost basis using Specific Lot Identification, Average Cost Basis, FIFO, or LIFO.</li>
               <li>We list the transaction in such a way to allow using FIFO or LIFO accounting principles.</li>
               <li>We generate a history of your transactions for you.</li>
+              <li>We generate reports on a weekly, monthly, or annual basis.</li>
             </ol>
           </div>
         </div>
