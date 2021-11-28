2ee32b6a34e30c44117e3ce2fd4c93a6eaac6429
Matt Blair <mblair@rivian.com>
2021-11-28 09:43:51 -0800

    try to fix viewport

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
         <title>SolCapture</title>
         <link rel="icon" href="/favicon.ico" />
         <script src="js/navigation.js" type="text/javascript"></script>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0;" />
       </Head>
 
       <Header />
