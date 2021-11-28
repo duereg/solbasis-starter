320c6ad46e3728caa8fe666059c909f5ae922870
Matt Blair <mblair@rivian.com>
2021-11-28 09:44:59 -0800

    Contact->Contact Us

--- components/routes.js
+++ components/routes.js
@@ -2,7 +2,7 @@ const routes = [
   {url: "/why-us", title: "Why Us" },
   {url: "/how-it-works", title: "How It Works" },
   {url: "/what-we-do", title: "What We Do" },
-  {url: "/contact", title: "Contact" },
+  {url: "/contact", title: "Contact Us" },
   {url: "/about-us", title: "About Us" },
 ];
 
