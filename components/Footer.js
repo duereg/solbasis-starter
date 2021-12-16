10dc350448d040c97931340c810b5664c3cc8dd3
Danny Agren <dannyagren@gmail.com>
2021-12-16 08:18:11 -0500

    Privacy Policy
    Added a privacy policy page and a link in the footer.

--- components/Footer.js
+++ components/Footer.js
@@ -21,7 +21,7 @@ export default function Footer() {
         </div>
       </div> */}
       <div className="copy pl-8 py-5 bg-tlgray-700 text-white text-md">
-        &copy; SolCapture 2021 All Rights Reserved.
+        &copy; SolCapture 2021 All Rights Reserved. <a href="/privacy-policy">Privacy Policy</a>
       </div>
     </footer>
 
---
+++ pages/privacy-policy.js
@@ -0,0 +1,84 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function About() {
+  return (
+
+    <div className="flex flex-col min-h-screen">
+    <main className="flex-grow">
+      <Head>
+        <title>SolCapture - Why Do You Need Our Service?</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script src="js/navigation.js" type="text/javascript"></script>
+      </Head>
+
+      <Header />
+      <div className="scroll-top rounded-full">
+        <span className="icon-keyboard_arrow_up text-2xl"></span>
+      </div>
+      <div className="mx-auto my-10 max-w-6xl">
+        <div className="mx-3 col-span-3 lg:col-span-2 px-2">
+          <h1 className="title text-5xl mb-4">Privacy Policy</h1>
+          <div className="content py-1">
+            <p>Last Updated: November 2021</p>
+              <p>This Privacy Policy has been compiled to provide users information about how SolCapture, LLC (“SolCapture,” “we,” “us” and “our”) handles personally identifiable information on SolCapture’s website and other services (“Site”). “Personally identifiable information” is information that identifies, relates to, describes, is capable of being associated with, or could reasonably be linked, directly or indirectly, with a particular individual. Please carefully read our Privacy Policy for information about how we collect, use, protect or otherwise handle your personally identifiable information. If you have a question about how your personally identifiable information is being used, you can contact us through our contact details at the end of the Privacy Policy.</p>
+              <p>By visiting this Site, or providing your personally identifiable information to us, you are accepting the practices described in this Privacy Policy and Terms of Use. Please exit this Site immediately if you do not accept this Privacy Policy or are accessing the Site in a country or other territory where use of the Site is not permitted.</p>
+              <p><b>WHAT PERSONALLY IDENTIFIABLE INFORMATION DO WE COLLECT?</b></p>
+              <p>The types of information we collect when you use the Site depend on the features you use on the Site or the information provided by Clients and End Users. We will handle your personally identifiable information in accordance with this Privacy Policy and describe the information collected below:
+              Identifiers, which may include name, address, telephone number, or email address. These are sourced directly from you, from our Clients, or indirectly from you (e.g., from observing your actions on the Site). They are used to fulfill or meet the reason you provided the information, to contact you in relation to our Site, to respond to an inquiry, for product and service improvement, or to screen for potential risk or fraud. For example, when you contact us, you may provide your first and last name, email address, telephone number, and other identifying information. 
+              Personal information categories contained in customer records, which may include name, address, telephone number, bank account number, credit card number, debit card number, or any other payment and financial information. We source this information directly from you. We use this information to fulfill or meet the reason you provided the information, to contact you in relation to our Site, to respond to an inquiry, to screen for potential risk or fraud, to provide the products and services that you have ordered or requested, to provide customer service, or marketing. For example, we need your payment information when you purchase services from us. 
+              Commercial information, which may include records of services purchased, obtained, or considered, or other purchasing or consuming histories or tendencies. We source this information directly from you. We use this information to fulfill or meet the reason you provided the information, to contact you in relation to our Site, to screen for potential risk or fraud, and to respond to an inquiry or process an order. For example, we keep track of the services you have obtained. 
+              Internet, technical or other similar network activity, which may include usage and browsing history; device information, including device properties; search history; information on your interaction with our Site; and error logs. We source this information directly or indirectly from you (e.g., from observing your actions on our Site). We use this information to fulfill or meet the reason you provided the information, to screen for potential risk or fraud, or to improve our services and Site. 
+              Inferences drawn from other personally identifiable information, which may include a profile reflecting a person’s preferences, interests, characteristics, psychological trends, predispositions, behavior, attitudes, intelligence, abilities, and aptitudes. We source this information directly or indirectly from you, (e.g., from observing your actions on our Site). We use this information to fulfill or meet the reason you provided the information, or to improve our services. </p>
+              <p>Your decision to provide any personally identifiable information is voluntary. However, please note that, if you do not provide certain personally identifiable information, we may not be able to accomplish some purposes outlined in this Privacy Policy and you may not be able to use or access certain services on our Site.</p>
+              <p><b>HOW DO WE COLLECT INFORMATION?</b></p>
+              <p>We collect information in the following ways:</p>
+              <p>Directly from you when you enter information on our Site. For example, your provision of your name, email address, wallet address, stake address, vote account address, and/or a validator PUB key. 
+              Indirectly from you when you visit our Site. For example, our Site collects information through cookies when you visit our Site. Please see the “Do We Use ‘Cookies’?” section below for more information. </p>
+              <p><b>HOW DO WE USE YOUR INFORMATION?</b></p>
+              <p>We may use the information we collect from you when you register or use certain Site features in the following ways:
+              To personalize user’s experience and to allow us to deliver the type of content, services, and product offerings in which you are most interested.
+              To improve our services and Site in order to better serve you.
+              To allow us to better service you in responding to your customer service requests.
+              To quickly process your transactions or respond to an inquiry.
+              To send periodic emails or marketing materials regarding our products and services.
+              For advertising and marketing purposes.
+              To fulfill or meet the reason you provided the information.
+              To notify you about changes to our service. </p>
+              <p><b>HOW DO WE PROTECT VISITOR INFORMATION?</b></p>
+              <p>The security of your information is very important to us and we will only collect personally identifiable information to the extent deemed reasonably necessary to serve you in view of our legitimate business purposes as set out above. Please understand, however, that no data transmissions over the Internet can be guaranteed to be 100% secure. Consequently, we cannot ensure or warrant the security of any information you transmit to us and you understand that any information that you transfer to us is done at your own risk. If we learn of a security systems breach we may attempt to notify you electronically so that you can take appropriate protective steps. By using the Site or providing information to us, you agree that we can communicate with you electronically regarding security, privacy and administrative issues relating to your use of the Site. We may post a notice via our Site if a security breach occurs. We may also send an email to you at the email address you have provided to us in these circumstances. Depending on where you live, you may have a legal right to receive notice of a security breach in writing.</p>
+              <p><b>DO WE USE ‘COOKIES’?</b></p>
+              <p>Like many websites, we use “cookies”, which are files stored on your computer's hard drive by your browser. We use cookies to ascertain which web pages are visited and how often, to make our Site more user friendly, and to give you a better experience when you return to the Site. For example, cookies allow us to save your password so you do not have to re-enter it every time you visit our Site. Most browsers accept cookies automatically but allow you to disable them. Please check your browser and browser settings to determine where these types of cookies are stored and whether and how they may be deleted. The Network Advertising Initiative also offers a means to opt-out of a number of advertising cookies. Please visit www.networkadvertising.org to learn more. In any event, if you reject our cookies, you may still use the Site, but you may be limited in some of the features. We may also work with service providers that use cookies and web beacons to collect information, and to serve advertisements to you across the Internet based on that information.
+              Google Analytics. To help facilitate the delivery of relevant content, we use Google Analytics. Google Analytics uses cookies to report on user interactions on our and others' websites. We use the data collected for optimizing marketing, refining advertising and/or programming strategies, and generally improving user experience. For more information about Google Analytics and how it collects and processes data, please visit: https://policies.google.com/technologies/partner-sites. Instructions on opting out of Google Analytics using a specific plug-in is available at the following link: https://tools.google.com/dlpage/gaoptout. Note that this opt-out is specific to Google activities and does not affect the activities of other ad networks or analytics providers that we may use.
+              Facebook Analytics. We also use Facebook Analytics to measure your use of our Site, tailor our Site to your interests, and improve our products and services. Please note, Facebook can connect this data with your Facebook account and use it for its own advertising purposes, in accordance with Facebook’s Data Policy which can be found at Facebook.com/about/privacy/. Please click here if you would like to withdraw your consent.</p>
+              <p><b>SHARING WITH THIRD PARTIES</b></p>
+              <p>We do not transfer to outside parties your personally identifiable information unless we provide you with advance notice. This does not include our own service providers such as website hosting partners and other parties who assist us in operating our Site, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our Site policies, or protect ours or others’ rights, property, or safety.
+              We may share or transfer your personally identifiable information in the course of any direct or indirect reorganization process including, but not limited to, mergers, acquisitions, divestitures, bankruptcies, and sales of all or a part of our assets. Your personally identifiable information may be shared following completion of such transaction and/or during the assessment process pending transfer. If transferred in such a case, your information would remain subject to this Privacy Policy or a policy that, at a minimum, protects your privacy to an equal degree as this Privacy Policy. </p>
+              <p><b>THIRD PARTY LINKS</b></p>
+              <p>Occasionally, at our discretion, we may include or offer third party products or services on our Site or provide links to other third party websites. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. To better protect your privacy, we recommend that you review the privacy policy of any third party website you visit. </p>
+              <p><b>FORUMS, CHAT ROOMS AND OTHER PUBLIC POSTING AREAS</b></p>
+              <p>Please note that any information you include in a message you post to any chat room, forum or other public posting area is available to others. If you don't want people to know your email address, for example, don't include it in any message that will be posted publicly. PLEASE BE EXTREMELY CAREFUL WHEN DISCLOSING ANY INFORMATION IN CHAT ROOMS, FORUMS AND OTHER PUBLIC POSTING AREAS. WE ARE NOT RESPONSIBLE FOR THE USE BY OTHERS OF THE INFORMATION THAT YOU DISCLOSE IN CHAT ROOMS, FORUMS AND OTHER PUBLIC POSTING AREAS.</p>
+              <p><b>CONSENT TO TRANSFER</b></p>
+              <p>If you are visiting our Site from a location outside of the U.S., your connection may be through and to servers located in the U.S. Any information you provide during your visit will be processed and maintained on our server and other internal systems located within the U.S. The U.S. may not have the same data protection laws as the country you are visiting from and may not afford many of the rights conferred upon data subjects in your country. You acknowledge you understand that by providing your personally identifiable information to us, your personally identifiable information (i) will be used for the uses identified above in accordance with this Privacy Policy, and (ii) may be transferred to the U.S. as indicated above, in accordance with applicable law.</p>
+              <p><b>DATA RETENTION</b></p>
+              <p>We will retain your information for as long as needed to provide our services and to comply with our accounting and legal obligations, resolve disputes, and enforce our agreements. </p>
+              <p><b>HOW DOES OUR SITE HANDLE DO NOT TRACK SIGNALS?</b></p>
+              <p>The “Do Not Track” (“DNT”) privacy preference is an option that may be made in some web browsers allowing you to opt-out of tracking by websites and online services. At this time, global standard DNT technology is not yet finalized and not all browsers support DNT. We therefore do not recognize DNT signals and do not respond to them.</p>
+              <p><b>CHILDREN</b></p>
+              <p>The Site is not intended for use by persons under the age of 13. We do not knowingly collect information from visitors under the age of 13 and, in the event that we learn that a person under the age of 13 has provided us with personally identifiable information, we will delete such personally identifiable information.
+              PRIVACY POLICY UPDATES
+              We may update this Privacy Policy from time to time. Whenever we make a change, we will post the updated policy at this Site and we encourage you to check our policy periodically. If we make a material change to this Privacy Policy, we will either continue to honor our prior policy for information that we collected when it was in force, or we will we will notify you by prominently posting the changes on our Site or by using the contact information you have on file with us. By continuing to use our Site you are agreeing to be bound by any changes or revisions made to this Privacy Policy.</p>
+
+
+              <p><b>CONTACT US</b></p>
+              <p>If you have any questions regarding this Privacy Policy you may contact us using the information below:
+              support@solcapture.com</p>
+          </div>
+        </div>
+      </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
