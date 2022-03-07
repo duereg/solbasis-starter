2c1060b34f663aeff809182fc3071ebb4f380dba
Matt Blair <mblair@rivian.com>
2022-03-06 18:56:50 -0800

    add pay link, button

--- pages/generate-report.js
+++ pages/generate-report.js
@@ -1,24 +1,52 @@
+import _ from 'lodash';
 import React from 'react';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 import TextField from '@mui/material/TextField';
+import { LoadingButton } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 
 import axios from 'axios';
+import { encodeURL, createQR } from '@solana/pay';
+import { PublicKey } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
 
 export default function Contact() {
   const [startDate, setStartDate] = React.useState(null);
   const [endDate, setEndDate] = React.useState(null);
   const [stakeAddress, setStakeAddress] = React.useState('');
   const [solanaUrl, setSolanaUrl] = React.useState('');
+  const [amount, setAmount] = React.useState(0);
+  const [loading, setLoading] = React.useState(false);
+  const ref = React.useRef(null);
 
   function onButtonClick() {
-    const url = `https://akyeck1ms6.execute-api.us-east-1.amazonaws.com/generateQuote?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&stakeAddress=${stakeAddress}&reportType=url`;
-    axios.get(url).then(results => setSolanaUrl(results.data));
+    setLoading(true);
+    const url = `https://akyeck1ms6.execute-api.us-east-1.amazonaws.com/generateQuote?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&stakeAddress=${stakeAddress}&reportType=json`;
+    axios.get(url).then(results => {
+      const json = results.data;
+      setAmount(json.amount);
+
+      const checkout = {...json,
+        recipient: new PublicKey(json.recipient),
+        amount: new BigNumber(json.amount),
+        reference: new PublicKey(json.reference), // this is the transaction signature!
+      };
+
+      return checkout;
+    }).then(checkout => {
+      const url = encodeURL(checkout);
+      setSolanaUrl(url);
+      return url;
+    }).then(url => {
+      const qrCode = createQR(url, 200);
+      qrCode.append(ref.current);
+      return qrCode;
+    });
   }
 
   return (
@@ -42,7 +70,6 @@ export default function Contact() {
                 <input
                   type="text"
                   onChange={(event) => {
-                    console.log('setStakeAddress', event);
                     setStakeAddress(event.target.value);
                   }}
                   placeholder="Stake Address"
@@ -78,9 +105,35 @@ export default function Contact() {
                 </LocalizationProvider>
               </div>
 
-              <input type="submit" value="Generate Quote" onClick={onButtonClick} className="px-8 py-2 duration-200 bg-gray-800 text-white cursor-pointer transition-colors hover:bg-gray-400"/>
+              {_.isEmpty(solanaUrl) &&
+                <div className="date-input">
+                  <LoadingButton
+                    onClick={onButtonClick}
+                    loading={loading}
+                    disabled={loading}
+                    variant="contained"
+                    className="px-8 py-2 duration-200 bg-gray-800 text-white cursor-pointer transition-colors hover:bg-gray-400"
+                  >
+                    Generate Quote
+                  </LoadingButton>
+                </div>
+              }
 
-              <div>{solanaUrl}</div>
+              {amount > 0 &&
+                <div className="date-input">
+                  <a href={solanaUrl}>
+                    <button type="button" href={solanaUrl} className="px-8 py-2 duration-200 bg-gray-800 text-white cursor-pointer transition-colors hover:bg-gray-400">
+                      Pay {amount} SOL
+                      <img alt="Pay for Quote with Sol" src="img/sol-pay.png" className="sol-pay" />
+                    </button>
+                  </a>
+                </div>
+              }
+
+              {!_.isEmpty(solanaUrl) &&
+                <div ref={ref} />
+              }
+              {/* <div>{solanaUrl}</div> */}
           </div>
         </div>
       </div>
